e6f9d4973baeb787591e94e1a8ea6c594d659f29
danchik31 <danchik31@rambler.ru>
2017-10-24 16:45:45 +0300

    Add button start

--- app/src/main/java/com/example/dkurdamosov/abap_test/Start.java
+++ app/src/main/java/com/example/dkurdamosov/abap_test/Start.java
@@ -3,18 +3,26 @@ package com.example.dkurdamosov.abap_test;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 
 /**
  * Created by dkurdamosov on 24.10.2017.
  */
 
-public class Start extends AppCompatActivity {
-
+public class Start extends AppCompatActivity implements View.OnClickListener{
+private Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start);
-
+        start=(Button) start.findViewById(R.id.start);
+        start.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+
+    }
 }
