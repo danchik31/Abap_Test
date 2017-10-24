e217d7b124e39b8b1f1912211e8183a2f2e6e389
danchik31 <danchik31@rambler.ru>
2017-10-24 16:33:48 +0300

    first

---
+++ app/src/main/java/com/example/dkurdamosov/abap_test/Start.java
@@ -0,0 +1,20 @@
+package com.example.dkurdamosov.abap_test;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dkurdamosov on 24.10.2017.
+ */
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+
+    }
+
+}
